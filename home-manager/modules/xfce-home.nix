ac09ce455056d55681e4c1e5d6513b6eb912d1b2
Ralf2oo2 <ralf2oo2@hotmail.com>
2024-06-17 23:35:53 +0200

    theming

--- home-manager/modules/xfce-home.nix
+++ home-manager/modules/xfce-home.nix
@@ -21,6 +21,24 @@
       '';
     };
   };
+  xfconf.settings = {
+    xfce4-desktop = {
+      "configver" = 2;
+      "panels" = "[<1>]";
+      "panels/panel-1/background-style" = 0;
+      "panels/panel-1/disable-struts" = false;
+      "panels/panel-1/enter-opacity" = 80;
+      "panels/panel-1/leave-opacity" = 80;
+      "panels/panel-1/length" = 100;
+      "panels/panel-1/length-adjust" = true;
+      "panels/panel-1/mode" = 0;
+      "panels/panel-1/nrows" = 1;
+      "panels/panel-1/position" = "p=8;x=96;y=1055";
+      "panels/panel-1/position-locked" = true;
+      "panels/panel-1/size" = 42;
+      "backdrop/screen0/monitorLVDS-1/workspace0/last-image" = "{pkgs.nixos-artwork.wallpapers.stripes-logo.gnomeFilePath}";
+    };
+  };
 
   programs.gpg.enable = true;
 
