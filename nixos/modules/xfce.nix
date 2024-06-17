44468b1a447343eb884cae885ecb9728ff5045d0
Ralf2oo2 <ralf2oo2@hotmail.com>
2024-06-17 19:12:00 +0200

    Fix error

--- nixos/modules/xfce.nix
+++ nixos/modules/xfce.nix
@@ -9,37 +9,7 @@
 
   environment = {
     systemPackages = with pkgs; [
-      pavucontrol
-      gnome.file-roller
-      gnome.gnome-disk-utility
-      wmctrl
-      xclip
-      xcolor
-      xdo
-      xdotool
-      xfce.catfish
-      xfce.gigolo
-      xfce.orage
-      xfce.xfburn
-      xfce.xfce4-appfinder
-      xfce.xfce4-clipman-plugin
-      xfce.xfce4-cpugraph-plugin
-      xfce.xfce4-dict
-      xfce.xfce4-fsguard-plugin
-      xfce.xfce4-genmon-plugin
-      xfce.xfce4-netload-plugin
-      xfce.xfce4-panel
-      xfce.xfce4-pulseaudio-plugin
-      xfce.xfce4-systemload-plugin
-      xfce.xfce4-weather-plugin
-      xfce.xfce4-whiskermenu-plugin
-      xfce.xfce4-xkb-plugin
       xfce4-docklite-plugin
-      xfce.xfdashboard
-      xorg.xev
-      xsel
-      xtitle
-      xwinmosaic
     ];
   };
 
