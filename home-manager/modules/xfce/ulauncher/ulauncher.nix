1ea685caf10a417438f85ab7b235b4ca510e4dce
Ralf2oo2 <ralf2oo2@hotmail.com>
2024-06-19 18:35:18 +0200

    More config

---
+++ home-manager/modules/xfce/ulauncher/ulauncher.nix
@@ -0,0 +1,6 @@
+{
+	xdg.configFile."ulauncher/settings.json" = { source = ./settings.json; };
+	xdg.configFile."ulauncher/user-themes/manifest.json" = { source = ./goxir-dark/manifest.json; };
+	xdg.configFile."ulauncher/user-themes/theme.css" = { source = ./goxir-dark/theme.css; };
+	xdg.configFile."ulauncher/user-themes/theme-gtk-3.20.css" = { source = ./goxir-dark/theme-gtk-3.20.css; };
+}
--- home-manager/modules/xfce/xfce-home.nix
+++ home-manager/modules/xfce/xfce-home.nix
@@ -7,6 +7,7 @@
 
   home.packages = with pkgs; [
     meslo-lgs-nf
+    notepadqq
   ];
 
   gtk = {
