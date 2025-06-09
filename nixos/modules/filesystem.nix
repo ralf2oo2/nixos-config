f451812e064a4ee449e4103b117936c57742d1db
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-06-10 00:10:59 +0200

    add more drives

--- nixos/modules/filesystem.nix
+++ nixos/modules/filesystem.nix
@@ -8,5 +8,32 @@
         "nofail" # Prevent system from failing if this drive doesn't mount
       ];
     };
+
+    "/mnt/m2GamingDrive" = {
+      device = "/dev/disk/by-uuid/1432159d-bcd7-4ffc-912b-7d847105b360";
+      fsType = "ntfs";
+      options = [
+        "users" # Allows any user to mount and unmount
+        "nofail" # Prevent system from failing if this drive doesn't mount
+      ];
+    };
+
+    "/mnt/dataDrive" = {
+      device = "/dev/disk/by-uuid/72aec4b8-cf7b-4d16-89c4-c18ca67b0288";
+      fsType = "ntfs";
+      options = [
+        "users" # Allows any user to mount and unmount
+        "nofail" # Prevent system from failing if this drive doesn't mount
+      ];
+    };
+
+    "/mnt/hddGamesSmallDrive" = {
+      device = "/dev/disk/by-uuid/46108dda-8428-4216-ae2f-143f81a920df";
+      fsType = "ntfs";
+      options = [
+        "users" # Allows any user to mount and unmount
+        "nofail" # Prevent system from failing if this drive doesn't mount
+      ];
+    };
   };
 }
