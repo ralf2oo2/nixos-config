8be32d3f291e6b5a125bb82147d86b81b34cff3f
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-06-13 23:32:00 +0200

    add p10k

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -1,8 +1,20 @@
 {
   programs.zsh = {
     enable = true;
-    autosuggestions.enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     
+    antidote = {
+      enable = true;
+      plugins = [''
+          romkatv/powerlevel10k
+      ''];
+    };
+  };
+
+  home.file.".p10k.zsh" = {
+    source = ./p10k.zsh;
+    executable = true;
   };
 }
