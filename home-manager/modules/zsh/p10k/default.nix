0d98c974e23e59a4398f428002e65130f664fbca
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-06-13 23:54:08 +0200

    try to fix theme

--- home-manager/modules/zsh/p10k/default.nix
+++ home-manager/modules/zsh/p10k/default.nix
@@ -5,5 +5,10 @@
       src = pkgs.zsh-powerlevel10k;
       file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
     }
+    {
+      name = "powerlevel10k-config";
+      src = ./p10k-config;
+      file = "p10k.zsh";
+    }
   ];
 }
