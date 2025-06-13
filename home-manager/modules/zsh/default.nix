608879ffd4c42a07ef54c563fde960182d8b5bb9
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-06-13 23:49:00 +0200

    fix maybe

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -1,20 +1,16 @@
 {
+  imports = [
+    ./p10k/default.nix
+  ];
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    
-    antidote = {
-      enable = true;
-      plugins = [''
-          romkatv/powerlevel10k
-      ''];
-    };
   };
 
-  home.file.".p10k.zsh" = {
-    source = ./p10k.zsh;
-    executable = true;
-  };
+  # home.file.".p10k.zsh" = {
+  #   source = ./p10k.zsh;
+  #   executable = true;
+  # };
 }
---
+++ home-manager/modules/zsh/p10k/default.nix
@@ -0,0 +1,9 @@
+{pkgs, ...} : {
+  programs.zsh.plugins = [
+    {
+      name = "powerlevel10k";
+      src = pkgs.zsh-powerlevel10k;
+      file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    }
+  ];
+}
