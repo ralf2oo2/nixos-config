81329630da3d79ed76df0fcd1a4266b1ac4adb86
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-10-20 22:32:42 +0200

    fix wallpaper

--- home-manager/modules/hyprland/hyprpaper/hyprpaper.nix
+++ home-manager/modules/hyprland/hyprpaper/hyprpaper.nix
@@ -1,18 +1,20 @@
-{
+{config, ...}:{
   home.file.".config/hypr/hyprpaper/wallpapers/bliss.jpg" = {
     source = ./wallpapers/bliss.jpg;
   };
-  services.hyprpaper.settings = {
-    ipc = "on";
-    splash = false;
-    splash_offset = 2.0;
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      ipc = "on";
+      splash = false;
+      splash_offset = 2.0;
 
-    preload =
-      [ "/home/<user>/.config/hypr/hyprpaper/wallpapers/bliss.jpg" ];
+      preload = [ "${config.home.homeDirectory}/.config/hypr/hyprpaper/wallpapers/bliss.jpg" ];
 
-    wallpaper = [
-      "DP-4,/home/<user>/.config/hypr/hyprpaper/wallpapers/bliss.jpg"
-      "HDMI-A-6,/home/<user>/.config/hypr/hyprpaper/wallpapers/bliss.jpg"
-    ];
+      wallpaper = [
+        "DP-4,${config.home.homeDirectory}/.config/hypr/hyprpaper/wallpapers/bliss.jpg"
+        "HDMI-A-6,${config.home.homeDirectory}/.config/hypr/hyprpaper/wallpapers/bliss.jpg"
+      ];
+    };
   };
 }
